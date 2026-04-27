be330e1f62e1f52e89ffc024d2230b6130e9c000
jbossiob <142426392+jbossiob@users.noreply.github.com>
2026-04-27 17:31:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -203,4 +203,5 @@ else:
 
 # 7. PIE DE PÁGINA
 st.markdown("---")
-st.caption("Fuente: Datos Abiertos - SuSalud")
+st.caption("Fuente: Datos Abiertos - SuSalud <br> Consulta B2 - Morbilidad en Consulta Ambulatoria: Esta tabla nos muestra el Reporte Consolidado de Morbilidad en Consulta Ambulatoria. Nos muestra los diagnósticos definitivos segun CIE 10 de los pacientes de una IPRESS en un mes determinado
+</br>")
